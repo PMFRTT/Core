cdd2154aac2eebb6a1dd9e37d5dc93bb36f925d4
Tim <jungeilgestim@gmail.com>
2021-03-05 10:52:04 +0100

    updated SettingsInventory build Method

--- src/main/java/settings/SettingsInventory.java
+++ src/main/java/settings/SettingsInventory.java
@@ -1,8 +1,9 @@
 package settings;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
@@ -13,7 +14,7 @@ public class SettingsInventory {
 
     PluginSettings pluginSettings;
     private final Inventory inventory;
-    private List<Integer> usableSlots = new ArrayList<Integer>(){{
+    private List<Integer> usableSlots = new ArrayList<Integer>() {{
         add(0);
         add(4);
         add(16);
@@ -34,15 +35,22 @@ public class SettingsInventory {
         int i = 0;
         if (!this.pluginSettings.getSettingsList().isEmpty()) {
             for (Setting setting : this.pluginSettings.getSettingsList()) {
-                Material type;
                 ItemStack itemStack = new ItemStack(setting.getMaterial());
                 ItemMeta itemMeta = itemStack.getItemMeta();
+                assert itemMeta != null;
                 itemMeta.setDisplayName(setting.getName());
+                itemMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
                 itemMeta.setLore(new ArrayList<String>() {{
                     add(setting.getDescription());
                 }});
+                if (setting.getSettingValue()) {
+                    itemMeta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 1, true);
+                } else {
+                    itemMeta.removeEnchant(Enchantment.PROTECTION_ENVIRONMENTAL);
+                }
                 itemStack.setItemMeta(itemMeta);
                 this.inventory.setItem(this.usableSlots.get(i), itemStack);
+                i++;
             }
         }
     }
